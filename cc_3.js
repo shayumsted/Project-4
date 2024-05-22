ff6cf745369c3a9e9c7645b1aab4d7aa38260ff5
Shamira Umstead <sumstead@usf.edu>
2024-05-22 10:45:55 -0400

    git commit -m "Override accelerate method in EV class to adjust speed and charge."

--- cc_3.js
+++ cc_3.js
@@ -4,7 +4,7 @@
 class Car {constructor(make, speed) {this.make = make; this.speed = speed;}}
 
 //Car's accelerated speed with a given value
-Car.prototype.accelrate = function(value) {
+Car.prototype.accelerate = function(value) {
     this.speed += value;
     console.log(this.make + 'accelerated to' + this.speed + ' km/h.');}
 
@@ -18,7 +18,12 @@ class EV extends Car {constructor(make, speed, charge) {super(make, speed); this
 
 // Charge Battery Method
 EV.prototype.chargeBattery = function(chargeTo) {this.charge = chargeTo;
-    console.log(`${this.make} battery charged to ${this.charge}%.`);
-}
+    console.log(`${this.make} battery charged to ${this.charge}%.`);}
+
+// Overriding the accelerating method into EV class
+EV.prototype.accelerate = function() {
+    super.accelerate(20); // Increases speed by 20 km/h
+    this.charge -= 1; // Decreases battery charge by 1%
+    console.log(`${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%.`);}
 
 //
